ed629ba2229f04c3653d4c739698ae89fc19ed7a
João Vitor <joaovitor12j@gmail.com>
2025-02-14 08:44:55 -0300

    feat(files): add src/config files
    add src/config files

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import Preloader from "./components/Pre";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer";
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes
+} from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+import "./style.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import About from "./components/About/About.tsx";
+
+function App() {
+    const [load, upadateLoad] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            upadateLoad(false);
+        }, 1200);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <Router>
+            <Preloader load={load} />
+            <div className="App" id={load ? "no-scroll" : "scroll"}>
+                <Navbar />
+                <ScrollToTop />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/sobre-mim" element={<About />} />
+
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
+    );
+}
+
+export default App;
